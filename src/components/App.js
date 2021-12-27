75737e12eb6b48840cccfb8901c370124aac82a5
Francis Dolan <francis.dolan@gmail.com>
2021-12-27 16:02:17 +0000

    Inital commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Comments</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
