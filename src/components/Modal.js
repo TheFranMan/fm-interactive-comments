a0d798007324837c5c39ae1e71d09d7a5f4d32e9
Francis Dolan <francis.dolan@gmail.com>
2022-01-29 15:57:29 +0000

    Wrapped modal in react-aria-modal for a11y

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,25 @@
+import AriaModalfrom from 'react-aria-modal'
+
 export default function Modal({deleteId, updateDeleteId, handleDelete}) {
     const handleCancel = () => {
         updateDeleteId(null)
     }
 
-    return (
-        <div className={ `modal-container${ deleteId !== null ? " open" : "" }` }>
-            <div className="modal">
-                <form className="modal__form" onSubmit={ handleDelete }>
-                    <h2 className="modal__form__heading f-xlrg">Delete comment</h2>
-                    <p className="modal__form__text f-reg">Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
-                    <input className="modal__form__btn modal__form__btn--cancel f-med" type="button" onClick={ handleCancel } value="No, Cancel" />
-                    <input className="modal__form__btn modal__form__btn--delete f-med" type="submit" value="Yes, Delete" />
-                </form>
-            </div>
-        </div>
-    )
+    let modal = <AriaModalfrom
+                    titleId="modal-heading"
+                    includeDefaultStyles="false"
+                    verticallyCenter="true"
+                    underlayColor="rgba(0,0,0, .6)"
+                >
+                    <div className="modal">
+                        <h2 id='modal-heading' className="modal__heading">Delete comment</h2>
+                        <p className="modal__description f-reg">Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
+                        <form className="modal__form" onSubmit={ handleDelete }>
+                            <input className="modal__form__btn modal__form__btn--cancel f-med" type="button" onClick={ handleCancel } value="No, Cancel" />
+                            <input className="modal__form__btn modal__form__btn--delete f-med" type="submit" value="Yes, Delete" />
+                        </form>
+                    </div>
+                </AriaModalfrom>
+
+    return deleteId !== null ? modal : false
 }
