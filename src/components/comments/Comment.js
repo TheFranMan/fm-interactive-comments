a3c00c5bbbe9efb3a2ccb35254f49cdfcc1e94c9
Francis Dolan <francis.dolan@gmail.com>
2022-01-30 14:52:18 +0000

    Added function to return created at string

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -81,6 +81,61 @@ const Comment = ({comment, updateDeleteId}) => {
         updateEditing(false)
     }
 
+    const timeSince = (ts) => {
+        let now = new Date()
+        let timeStamp = new Date(ts * 1000)
+        let secondsPast = ( now.getTime() - timeStamp.getTime() ) / 1000;
+
+        let min = 60
+        let hour = min * 60
+        let day = hour * 24
+        let week = day * 7
+        let month = week * 4
+        let year = month * 12
+
+        let units = {
+            second: "second",
+            minute: "minute",
+            hour: "hour",
+            day: "day",
+            week: "week",
+            month: "month",
+            year: "year"
+        }
+
+        let displayNum = 0
+        let displayUnit = ""
+
+        if(secondsPast < min){
+            displayNum = secondsPast
+            displayUnit = units.second
+        } else if(secondsPast < hour){
+            displayNum = parseInt(secondsPast/min)
+            displayUnit = units.minute
+        } else if(secondsPast <= day){
+            displayNum = parseInt(secondsPast/hour)
+            displayUnit = units.hour
+        } else if(secondsPast <= week){
+            displayNum = parseInt(secondsPast/day)
+            displayUnit = units.day
+        } else if(secondsPast <= month){
+            displayNum = parseInt(secondsPast/week)
+            displayUnit = units.week
+        } else if(secondsPast <= year) {
+            displayNum = parseInt(secondsPast/month)
+            displayUnit = units.month
+        } else {
+            displayNum = parseInt(secondsPast/year)
+            displayUnit = units.year
+        }
+
+        if ( 1 < displayNum ) {
+            displayUnit += "s"
+        }
+
+        return displayNum + " " + displayUnit + " ago"
+    }
+
     let editComment = <textarea className='comment__body f-reg' defaultValue={ comment.content } ref={ editRef } ></textarea>
 
     return (
@@ -89,7 +144,7 @@ const Comment = ({comment, updateDeleteId}) => {
                 <header className='comment__heading'>
                     <img src={comment.user.image.png} className='comment__heading__avatar' alt='' />
                     <span className='comment__heading__name f-lrg'>{ comment.user.username }</span>
-                    <span className='comment__heading__created f-reg'>{ comment.createdAt }</span>
+                    <span className='comment__heading__created f-reg'>{ timeSince(comment.createdAt) }</span>
                 </header>
                 { !editing ?
                     <div className='comment__body f-reg'>{ comment.content }</div>
